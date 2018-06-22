7ff79d6946269960e28a02ec46c1986b240af7e9
Ther Matthews <emeraldvision@gmail.com>
2018-06-21 20:00:28 -0500

    completed "part 1: persisting a single class"

---
+++ src/main/java/org/launchcode/models/Category.java
@@ -0,0 +1,36 @@
+package org.launchcode.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min = 3, max = 15, message = "Must be between 3-15 characters")
+    @Column(unique = true)
+    private String name;
+
+    public Category() {}
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
