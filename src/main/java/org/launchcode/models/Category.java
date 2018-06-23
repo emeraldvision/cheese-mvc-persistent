837275889ba927bc92acbd6ce4823c77f6b70999
Ther Matthews <emeraldvision@gmail.com>
2018-06-23 11:24:01 -0500

    completed "part 2: setting up a one-to-many relationship"

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -3,9 +3,15 @@ package org.launchcode.models;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Category {
+
+    /******
+     * Fields
+     ******/
 
     @Id
     @GeneratedValue
@@ -16,11 +22,25 @@ public class Category {
     @Column(unique = true)
     private String name;
 
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
+
+
+    /******
+     * Constructors
+     ******/
+
     public Category() {}
 
     public Category(String name) {
         this.name = name;
     }
+
+
+    /******
+     * Getters and Setters
+     ******/
 
     public int getId() {
         return id;
